018a0447732d732b2958471d3832be469273a289
Waylon <waylon_brown@hotmail.com>
2018-02-13 17:24:34 -0600

    Leave work 2/13/18

--- FantasyGrease/ViewModels/StatusBoxPlayerViewModel.cs
+++ FantasyGrease/ViewModels/StatusBoxPlayerViewModel.cs
@@ -12,35 +12,6 @@ namespace FantasyGrease.ViewModels
 {
 	public class StatusBoxPlayerViewModel
 	{
-		StatusBoxPlayerModel statusBoxPlayerModel = new StatusBoxPlayerModel();
-        private App app = App.Current as App;
-		EliteAPI apiHook;
-/*
-		private void Update()
-		{
-			apiHook = app.mainHook.apiHook;
-			statusBoxPlayerModel.Hp = apiHook.Player.HP.ToString();
-			statusBoxPlayerModel.Mp = apiHook.Player.MP.ToString();
-		}
-
-        public void UpdateTimerStart()
-        {
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Tick += new EventHandler(timer_tick);
-            timer.Interval = new TimeSpan(0, 0, 1);
-            timer.Start();
-        }
-
-        private void timer_tick(object sender, EventArgs e)
-        {
-            apiHook = app.mainHook.apiHook;
-            if (apiHook != null)
-            {
-                //Update();
-            }
-        }
-
-    */
-
+	
     }
 }
