c9ce2d03f803afb24dffe0b19a427a989579aaf5
Waylon Brown <waylon_brown@hotmail.com>
2018-03-12 21:40:11 -0500

    Done for the night. 3/12/18 Added Unity View Model. Attempted databinding on unity view.

---
+++ FantasyGrease/ViewModels/UnityViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyGrease.ViewModels
+{
+    class UnityViewModel : INotifyPropertyChanged
+	{
+		public bool isRunning;
+
+		public string test
+		{
+			get { return "test"; }
+			set { }
+		}
+
+		public UnityViewModel()
+		{
+			isRunning = false;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
